f5e3ae2bf505e0e19567c31f369643e220129ebb
laverntius <narugo901@gmail.com>
2018-02-13 21:27:51 +0000

    lolzor

---
+++ script.js
@@ -0,0 +1,13 @@
+function travelToVenice(){
+    document.getElementById("homepage").setAttribute("visible", "false");
+    document.getElementById("spacevideo").setAttribute("muted", "true")
+     document.getElementById("scene2").setAttribute("visible", "true");
+      document.getElementById("backbutton").setAttribute("visible", "true")
+}
+
+function traveltoHomepage(){
+    document.getElementById("homepage").setAttribute("visible", "false");
+    document.getElementById("spacevideo").setAttribute("muted", "true")
+     document.getElementById("scene2").setAttribute("visible", "true");
+     document.getElementById("backbutton").setAttribute("visible", "false")
+}
